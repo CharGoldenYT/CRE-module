ee5213932a95c143f9179a49b5eb86cea7a43661
Anny (Char) <73309364+CharGoldenYT@users.noreply.github.com>
2024-11-07 21:21:10 -0500

    Update LocalSettings.hx

--- utils/LocalSettings.hx
+++ utils/LocalSettings.hx
@@ -1,6 +1,48 @@
 package cre.utils;
 
-class LocalSettings
+import cre.utils.Constants.NoteColors;
+
+/**
+ * This class handles actual setting of... settings.
+ */
+class LocalSettings extends DefaultSettings
+{
+	public function loadDefaults():DefaultSettings
+	{
+		var instance:DefaultSettings = new DefaultSettings();
+
+		for (key in Reflect.fields(instance))
+		{
+			Reflect.setField(this, key, Reflect.field(instance, key));
+		}
+		return instance;
+	}
+
+	/**
+	 * Sets a setting to a certain value if it exists.
+	 * @param setting The setting to change
+	 * @param value The value
+	 */
+	public function setSetting(setting:String, value:String):Void
+	{
+		if (Reflect.fields(this).contains(setting))
+		{
+			try
+			{
+				Reflect.setField(this, setting, value);
+			}
+			catch (e:Dynamic)
+			{
+				trace('SETTING $setting DOES NOT EXIST!');
+			}
+		}
+	}
+}
+
+/**
+ * This class holds all the default settings.
+ */
+class DefaultSettings
 {
 	/**
 	 * Controls whether to use antialiasing for sprites.
@@ -11,4 +53,13 @@ class LocalSettings
 	public static var sickWindow:Int = 45;
 	public static var goodWindow:Int = 90;
 	public static var badWindow:Int = 135;
+
+	public static var noteColors:Array<NoteColors> = [
+		Constants.noteColors_Left,
+		Constants.noteColors_Down,
+		Constants.noteColors_Up,
+		Constants.noteColors_Right
+	];
+
+	public function new() {}
 }
